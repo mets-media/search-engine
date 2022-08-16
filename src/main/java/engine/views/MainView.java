8a54b472deaa06d43fb38645c92102416034c3da
Александр <mets.alexandr@gmail.com>
2022-08-16 22:15:49 +0300

    Итоговый проект. SearchEngine. Первый этап.

--- src/main/java/engine/views/MainView.java
+++ src/main/java/engine/views/MainView.java
@@ -1,6 +1,9 @@
 package engine.views;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
@@ -12,6 +15,7 @@ import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.NativeButtonRenderer;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
@@ -23,10 +27,12 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 @Route
 @Getter
 public class MainView extends AppLayout {
+
     @Autowired
     SiteRepository siteRepository;
 
@@ -35,6 +41,7 @@ public class MainView extends AppLayout {
 
     @Autowired
     Parser parser;
+
 
     public MainView() {
 
@@ -68,9 +75,19 @@ public class MainView extends AppLayout {
         List<Site> sites = siteRepository.findAll();
 
         if (!sites.isEmpty()) {
+            //grid.setSelectionMode(Grid.SelectionMode.NONE);
+            grid.setSelectionMode(Grid.SelectionMode.SINGLE);
+
+            grid.addItemClickListener(
+                    event -> {
+                        showMessage("Clicked Item: " + event.getItem(), 1000, Notification.Position.MIDDLE);
+                    });
+
             grid.getStyle()
                     .set("font-size", "var(--lumo-font-size-xs)")
                     .set("margin", "0");
+
+
 
             //Cтолбцы в нужном порядке
             //grid.addColumn(Site::getUrl).setHeader("Сайт");
@@ -91,14 +108,32 @@ public class MainView extends AppLayout {
 
             //Кнопка редактирования
             grid.addColumn(new NativeButtonRenderer<Site>("Изменить", site -> {
-                UI.getCurrent().navigate(ManageSite.class, site.getId());
+                //notifyShow("Id: " + grid.getSelectedItems().stream().findFirst().get().getId(), 2000);
+                //Возможно при MULTISELECT в Grid
+                //setContent(getModifyComponent(grid.getSelectedItems().stream().findFirst().get().getId()));
+                setContent(getModifyComponent(site.getId()));
             })).setAutoWidth(true).setFlexGrow(0);
+
+            //Кнопка редактирования первый вариант
+//            grid.addColumn(new NativeButtonRenderer<Site>("Изменить", site -> {
+//                UI.getCurrent().navigate(ManageSite.class, site.getId());
+//            })).setAutoWidth(true).setFlexGrow(0);
+
+            Button createButton = new Button("Добавить сайт");
+            createButton.getStyle()
+                    .set("font-size", "var(--lumo-font-size-xs)")
+                    .set("margin", "0");
+
+            createButton.addClickListener(buttonClickEvent -> {
+                setContent(getModifyComponent(0l));
+            });
 
             //Кнопка удаления
             grid.addColumn(new NativeButtonRenderer<Site>("Удалить", site -> {
                 Dialog dialog = new Dialog();
                 Button confirm = new Button("Удалить");
                 Button cancel = new Button("Отмена");
+
                 dialog.add("Вы уверены что хотите удалить сайт?");
                 dialog.add(confirm);
                 dialog.add(cancel);
@@ -125,15 +160,98 @@ public class MainView extends AppLayout {
         //layout.setMaxWidth(600, Unit.PIXELS);
 
         Grid<Site> grid = new Grid<>();
-        RouterLink linkCreate = new RouterLink("Добавить сайт", ManageSite.class, 0l);
-        linkCreate.getStyle()
+        //Первый вариант...
+//        RouterLink linkCreate = new RouterLink("Добавить сайт", ManageSite.class, 0l);
+//        linkCreate.getStyle()
+//                .set("font-size", "var(--lumo-font-size-xs)")
+//                .set("margin", "0");
+//        layout.add(linkCreate);
+
+        Button createButton = new Button("Добавить сайт");
+        createButton.getStyle()
                 .set("font-size", "var(--lumo-font-size-xs)")
                 .set("margin", "0");
-        layout.add(linkCreate);
+
+        createButton.addClickListener(buttonClickEvent -> {
+            setContent(getModifyComponent(0l));
+        });
+        layout.add(createButton);
+
         layout.add(grid);
         fillGrid(grid);
         return layout;
     }
+
+    private VerticalLayout getModifyComponent(Long idSite) {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.START);
+
+        TextField urlText = new TextField("Редактирование:");
+        if (idSite == 0) {
+            urlText.setLabel("Добавить сайт:");
+        }
+
+        Button saveButton = new Button("Сохранить");
+
+
+        layout.setMaxWidth(700, Unit.PIXELS);
+
+        layout.getStyle()
+                .set("font-size", "var(--lumo-font-size-xs)")
+                .set("margin", "0");
+        urlText.getStyle()
+                .set("font-size", "var(--lumo-font-size-xs)")
+                .set("margin", "0");
+        saveButton.getStyle()
+                .set("font-size", "var(--lumo-font-size-xs)")
+                .set("margin", "0");
+
+        //Все элементы на форму
+        layout.add(urlText, saveButton);
+
+        fillModifyComponent(idSite, urlText, saveButton);
+
+        return layout;
+    }
+
+    private void fillModifyComponent(Long id, TextField textField, Button button) {
+        if (!id.equals(0)) {
+            Optional<Site> site = siteRepository.findById(id);
+            site.ifPresent(x -> {
+                textField.setValue(x.getUrl());
+            });
+        }
+        button.addClickListener(clickEvent -> {
+            //Создадим объект Site получив значение id
+            if (!textField.getValue().equals("")) {
+                Site site = new Site();
+                if (!id.equals(0l)) {
+                    site.setId(id);
+                }
+                site.setUrl(textField.getValue());
+                siteRepository.save(site);
+
+                Notification notification = new Notification(id.equals(0l) ? "Сайт успешно добавлен" : "Изменения внесены", 1000);
+                notification.setPosition(Notification.Position.MIDDLE);
+                notification.addDetachListener(detachEvent -> {
+                    //UI.getCurrent().navigate(MainView.class);
+                    setContent(getSitesComponent());
+                });
+                //formLayout.setEnabled(false);
+                notification.open();
+            } else {
+                showMessage("Вы ничего не внесли...", 1000, Notification.Position.MIDDLE);
+                setContent(getSitesComponent());
+            }
+        });
+    }
+
+    private void showMessage(String text, int duration, Notification.Position position) {
+        Notification notification = new Notification(text, duration);
+        notification.setPosition(position);
+        notification.open();
+    }
+
 
 }
 
